987d4b97f53c21724ee71b7a3f3c1d138a416b1a
imdixitthakur <dixitthakur0021@gmail.com>
2024-09-09 10:36:49 +0530

    translator code modefied

--- translator/translator.py
+++ translator/translator.py
@@ -5,9 +5,10 @@ input_text=input(" enter the text in english :")
 
 trans = EngtoHindi(message=input_text)
 text_in_hindi=trans.convert
-print(text_in_hindi)
-
-with open('search_history.txt',mode='a') as search_file:
-
+with open('./search_history.txt',mode='a') as search_file:
     text=input_text
     search_file.write(text)
+
+
+print(text_in_hindi)
+
