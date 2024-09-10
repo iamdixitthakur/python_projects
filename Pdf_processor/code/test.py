145576dac19469e6fd1e6adc89923262a358cf47
iamdixitthakur <125883677+iamdixitthakur@users.noreply.github.com>
2024-09-10 17:25:50 +0530

    Update test.py

--- Pdf_processor/code/test.py
+++ Pdf_processor/code/test.py
@@ -1,10 +1,1 @@
-import glob 
-import os
 
-# Specify the directory path
-
-directory = os.getcwd()
-
-# Get all files in the directory
-files = [os.path.basename(f) for f in glob.glob(f"{directory}/*")]
-print(files)
